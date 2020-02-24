be6068a1132dd77fd8ed6d34a3354b162d2ea5ca
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-24 16:53:52 -0500

    fills out timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+import decreaseTimer from '../actions/decreaseTimer'
+import { Segment, Header } from 'semantic-ui-react'
 
 class Timer extends React.Component {
 
+    interval = null
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.props.decreaseTimer()
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    render() {
+        return <Segment>
+            <Header as='h1'>{this.props.timeRemaining}</Header>
+            <Header.Subheader>Seconds remaining</Header.Subheader>
+        </Segment>
+    }
+
 }
 
-export default Timer
+const mapStateToProps = state => {
+    return {
+        ...state.timer
+    }
+}
+
+export default connect(mapStateToProps, {decreaseTimer})(Timer)
