4d6fdbd43668df0b6053e7017579fd7cce313655
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-27 16:28:52 -0500

    fills out ScoresTable

--- src/containers/ScoresTable.js
+++ src/containers/ScoresTable.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import {Table} from 'semantic-ui-react'
+import {connect} from 'react-redux'
+
+import Score from '../components/Score'
 
 const ScoresTable = props => {
+    return <Table celled>
+        <Table.Header>
+            <Table.Row>
+                <Table.HeaderCell>Genre</Table.HeaderCell>
+                <Table.HeaderCell>Score</Table.HeaderCell>
+            </Table.Row>
+        </Table.Header>
 
+        <Table.Body>
+            {
+                props.scores.map(s => {
+                    return <Table.Row>
+                        <Score useGenre score={s} />
+                    </Table.Row>
+                })
+            }
+        </Table.Body>
+    </Table>
 }
 
-export default ScoresTable
+const mapStateToProps = state => {
+    return {
+        scores: state.profileData.scores
+    }
+}
+
+export default connect(mapStateToProps)(ScoresTable)
