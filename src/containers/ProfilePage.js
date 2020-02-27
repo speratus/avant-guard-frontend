a0b2594b503d39f34ab225d38ada6abd3fdd530d
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-27 14:47:30 -0500

    adds genre scores fetch

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -5,7 +5,16 @@ import {BASE_URL} from '../index'
 class ProfilePage extends React.Component {
 
     componentDidMount() {
-        fetch(/* finish this fetch request! */)
+        const {userId} = this.props.match.params
+        fetch(BASE_URL+`/users/${userId}/genre_scores`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Access-Token': localStorage.getItem('token')
+            }
+        }).then(res=> res.json()).then(scores => {
+            console.log(scores)
+        })
     }
 
 }
