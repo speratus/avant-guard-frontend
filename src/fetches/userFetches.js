70d5fb586dfdaef7af8d6ba1d5ceba6f262eafa0
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-28 11:46:25 -0500

    adds a post for creating a friendship

--- src/fetches/userFetches.js
+++ src/fetches/userFetches.js
@@ -10,3 +10,18 @@ export function fetchUserList() {
         }
     }).then(res=>res.json())
 }
+
+export function postNewFriendship(userId, friendId) {
+    return fetch(BASE_URL+'/friendships', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'Access-Token': localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            friender_id: userId,
+            friended_id: friendId
+        })
+    }).then(res=>res.json())
+}
