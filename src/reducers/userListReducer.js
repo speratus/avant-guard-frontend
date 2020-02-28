c3d56155b98a7474cbee8a7dce3e3ff5b5e6cf82
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-28 10:22:35 -0500

    adds new action to user list reducer

--- src/reducers/userListReducer.js
+++ src/reducers/userListReducer.js
@@ -1,15 +1,29 @@
 import factory from 'redux-reducer-builder'
 
 import loadUserData from '../actions/loadUserData'
+import updateUserSearch from '../actions/updateUserSearch'
 
-const initialState = []
+const initialState = {
+    users: [],
+    search: ''
+}
 
 const builder = factory()
 
 builder.setInitialState(initialState)
 
 builder.addAction(loadUserData, (state, action) => {
-    return action.users
+    return {
+        ...state,
+        users: action.users
+    }
+})
+
+builder.addAction(updateUserSearch, (state, action) => {
+    return {
+        ...state,
+        search: action.search
+    }
 })
 
 export default builder.buildReducer()
