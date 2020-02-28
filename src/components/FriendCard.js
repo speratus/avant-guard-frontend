33e9263dc1a6d381fff8b672841837c9a5cfdb7a
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-28 14:13:42 -0500

    adds friend card

---
+++ src/components/FriendCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Card, Button } from 'semantic-ui-react'
+import {withRouter} from 'react-router-dom'
+
+const FriendCard = props => {
+    const {id, name, username} = props.user
+    return <Card>
+        <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Meta>{username}</Card.Meta>
+        </Card.Content>
+
+        <Card.Content extra>
+            <Button basic
+                onClick={() => {
+                    props.history.push({
+                        pathname: `/users/${id}`
+                    })
+                }}
+            >
+                View Profile
+            </Button>
+        </Card.Content>
+    </Card>
+}
+
+export default withRouter(FriendCard)
