e30e118306c599013d7cf9d6356456be15684f8b
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-28 14:17:39 -0500

    finishes FriendsListContainer

--- src/containers/FriendsListContainer.js
+++ src/containers/FriendsListContainer.js
@@ -4,6 +4,9 @@ import {connect} from 'react-redux'
 import loadFriendsList from '../actions/loadFriendsList'
 import {getFriends} from '../fetches/userFetches'
 
+import FriendCard from '../components/FriendCard'
+import { Container, Grid } from 'semantic-ui-react'
+
 class FriendsListContainer extends React.Component {
 
     componentDidMount() {
@@ -11,6 +14,22 @@ class FriendsListContainer extends React.Component {
             console.log(friends)
             this.props.loadFriendsList(friends)
         })
+    }
+
+    render() {
+        return <Container style={{marginTop: '3em'}}>
+            <Grid columns={5}>
+                <Grid.Row>
+                    {
+                        this.props.friendsList.map(f => (
+                            <Grid.Column>
+                                <FriendCard user={f} />
+                            </Grid.Column>
+                        ))
+                    }
+                </Grid.Row>
+            </Grid>
+        </Container>
     }
 }
 
