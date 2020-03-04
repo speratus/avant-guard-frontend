2a5c4b7b2ab92fa40cf2a162bd87ee93bbb1ddaf
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-03-04 14:04:12 -0500

    patches logout crashing bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,8 @@ import { Link, withRouter } from 'react-router-dom';
 import {connect} from 'react-redux'
 
 import setLogout from '../actions/logout'
+import resetGamePhase from '../actions/resetGamePhase'
+import resetGameState from '../actions/resetGameState'
 
 const renderLoggedOutView = () => {
     return <React.Fragment>
@@ -37,6 +39,8 @@ const loggedInView = props =>{
 const logout = props => {
     localStorage.clear()
     props.setLoggedOut()
+    props.resetGamePhase()
+    props.resetGameState()
     props.history.push({
         pathname: "/",
         state: {loggedIn: false}
@@ -64,4 +68,13 @@ const mapStateToProps = state => {
     }
 }
 
-export default withRouter(connect(mapStateToProps, {setLoggedOut: setLogout})(Navbar))
+export default withRouter(
+    connect(
+        mapStateToProps, 
+        {
+            setLoggedOut: setLogout, 
+            resetGamePhase, 
+            resetGameState
+        }
+    )(Navbar)
+)
